c10ba09dbc40a0bac65b2c1c203339656b9e5d25
Mike Lewis <mike@updroid.com>
2015-06-11 15:28:23 -0700

    Moved websocket code into separate file

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-import "io"
 import "flag"
 import "fmt"
 import "net"
@@ -9,74 +8,9 @@ import "os"
 import "os/exec"
 import "strings"
 import "strconv"
-import "unicode/utf8"
 
 import "github.com/kr/pty"
 import "github.com/creack/goterm/win"
-import "github.com/gorilla/websocket"
-
-// Copy everything from the pty master to the websocket.
-func handleOutput(ptym *os.File, conn *websocket.Conn) {
-	buf := make([]byte, 512)
-	var payload, overflow []byte
-	// TODO: more graceful exit on socket close / process exit.
-	for {
-		n, err := ptym.Read(buf)
-		if err != nil {
-			fmt.Println("failed to read from pty master: ", err)
-			return
-		}
-
-		// Empty the overflow from the last read into the payload first.
-		payload = append(payload[0:], overflow...)
-		overflow = nil
-		// Then empty the new buf read into the payload.
-		payload = append(payload, buf[:n]...)
-
-		// Strip out any incomplete utf-8 from current payload into overflow.
-		for !utf8.Valid(payload) {
-			overflow = append(overflow[:0], append(payload[len(payload)-1:], overflow[0:]...)...)
-			payload = payload[:len(payload)-1]
-		}
-
-		// Send out the finished payload as long as it's not empty.
-		if len(payload) >= 1 {
-			err = conn.WriteMessage(websocket.BinaryMessage, payload[:len(payload)])
-			if err != nil {
-				fmt.Println("failed to send bytes on websocket: ", err)
-				return
-			}
-		}
-
-		// Empty the payload.
-		payload = nil
-	}
-}
-
-// Read from the websocket, copying to the pty master.
-func handleInput(ptym *os.File, conn *websocket.Conn) {
-	for {
-		mt, payload, err := conn.ReadMessage()
-		if err != nil {
-			if err != io.EOF {
-				fmt.Println("conn.ReadMessage failed: ", err)
-				return
-			}
-		}
-
-		// The client has likely disconnected.
-		if mt == -1 {
-			return
-		}
-
-		if mt == websocket.BinaryMessage {
-			ptym.Write(payload)
-		} else {
-			fmt.Println("invalid message type: ", mt)
-			return
-		}
-	}
-}
 
 func setPtySize(ptym *os.File, size string) {
 	sizeArr := strings.Split(size, "x")
@@ -100,51 +34,11 @@ func start() (*os.File, *exec.Cmd) {
 	return ptym, cmd
 }
 
-func stop(ptym *os.File, cmd *exec.Cmd, conn *websocket.Conn) {
+func stop(ptym *os.File, cmd *exec.Cmd) {
 	ptym.Close()
-	conn.Close()
 	cmd.Wait()
 }
 
-func ptyHandler(w http.ResponseWriter, r *http.Request, sizeFlag string) {
-	var upgrader = websocket.Upgrader{
-		ReadBufferSize:  1,
-		WriteBufferSize: 1,
-		CheckOrigin: func(r *http.Request) bool {
-			return true
-		},
-	}
-
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Println("websocket upgrade failed: ", err)
-	}
-
-	ptym, cmd := start()
-	setPtySize(ptym, sizeFlag)
-
-	go func() {
-		handleOutput(ptym, conn)
-	}()
-
-	go func() {
-		handleInput(ptym, conn)
-	}()
-
-	// Listen for a new winsize on stdin.
-	for {
-		var newSize string
-		_, scanErr := fmt.Scanln(&newSize)
-		if scanErr != nil {
-			fmt.Println("scan failed: ", scanErr)
-		}
-
-		setPtySize(ptym, newSize)
-		fmt.Println("new size: ", newSize)
-	}
-
-	stop(ptym, cmd, conn)
-}
 
 func main() {
 	addrFlag := flag.String("addr", ":0", "IP:PORT or :PORT address to listen on")
---
+++ websocket.go
@@ -0,0 +1,117 @@
+package main
+
+import "io"
+import "fmt"
+import "net/http"
+import "os"
+import "unicode/utf8"
+
+import "github.com/gorilla/websocket"
+
+// Copy everything from the pty master to the websocket.
+func handleOutputWs(ptym *os.File, conn *websocket.Conn) {
+	buf := make([]byte, 512)
+	var payload, overflow []byte
+	// TODO: more graceful exit on socket close / process exit.
+	for {
+		n, err := ptym.Read(buf)
+		if err != nil {
+			fmt.Println("failed to read from pty master: ", err)
+			return
+		}
+
+		// Empty the overflow from the last read into the payload first.
+		payload = append(payload[0:], overflow...)
+		overflow = nil
+		// Then empty the new buf read into the payload.
+		payload = append(payload, buf[:n]...)
+
+		// Strip out any incomplete utf-8 from current payload into overflow.
+		for !utf8.Valid(payload) {
+			overflow = append(overflow[:0], append(payload[len(payload)-1:], overflow[0:]...)...)
+			payload = payload[:len(payload)-1]
+		}
+
+		// Send out the finished payload as long as it's not empty.
+		if len(payload) >= 1 {
+			err = conn.WriteMessage(websocket.BinaryMessage, payload[:len(payload)])
+			if err != nil {
+				fmt.Println("failed to send bytes on websocket: ", err)
+				return
+			}
+		}
+
+		// Empty the payload.
+		payload = nil
+	}
+}
+
+// Read from the websocket, copying to the pty master.
+func handleInputWs(ptym *os.File, conn *websocket.Conn) {
+	for {
+		mt, payload, err := conn.ReadMessage()
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("conn.ReadMessage failed: ", err)
+				return
+			}
+		}
+
+		// The client has likely disconnected.
+		if mt == -1 {
+			return
+		}
+
+		if mt == websocket.BinaryMessage {
+			ptym.Write(payload)
+		} else {
+			fmt.Println("invalid message type: ", mt)
+			return
+		}
+	}
+}
+
+func ptyHandler(w http.ResponseWriter, r *http.Request, sizeFlag string) {
+	var upgrader = websocket.Upgrader{
+		ReadBufferSize:  1,
+		WriteBufferSize: 1,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("websocket upgrade failed: ", err)
+	}
+
+	ptySetup(conn, sizeFlag)
+}
+
+func ptySetup(ws *websocket.Conn, sizeFlag string) {
+	ptym, cmd := start()
+	setPtySize(ptym, sizeFlag)
+
+	go func() {
+		handleOutputWs(ptym, ws)
+	}()
+
+	go func() {
+		handleInputWs(ptym, ws)
+	}()
+
+	// Listen for a new winsize on stdin.
+	for {
+		var newSize string
+		_, scanErr := fmt.Scanln(&newSize)
+		if scanErr != nil {
+			fmt.Println("scan failed: ", scanErr)
+		}
+
+		setPtySize(ptym, newSize)
+		fmt.Println("new size: ", newSize)
+	}
+
+	stop(ptym, cmd)
+	ws.Close()
+}
